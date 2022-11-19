3358ee23d55bb0c998b02b6c6f08d6dd34ba7744
David-Ostos <davidostos2@gmail.com>
2022-11-19 07:57:50 -0400

    archivo comersionTipos

---
+++ Aula1/04-ConversionTipos.js
@@ -0,0 +1,15 @@
+//tipos de datos
+//alfanuemrico
+var nombrePasajero = "Pedro Silva";
+
+console.log(nombrePasajero);
+
+nombrePasajero = "Ramon silva";
+
+console.log(nombrePasajero);
+
+//numerico
+
+let valorBoleto = 1650;
+
+console.log(valorBoleto);
